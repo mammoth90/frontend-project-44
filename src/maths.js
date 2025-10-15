8b3876c1a32a22063571560f23ce763d77e23cba
mammoth90 <max.mamontov90@gmail.com>
2025-10-15 10:57:35 +0500

    Sonar warnin fix: math.js

--- src/maths.js
+++ src/maths.js
@@ -1,5 +1,5 @@
 const max = 100;
 
-export const getInt = () => Math.floor(Math.random() * max);
+export const getInt = () => Math.floor(Math.random() * max); // NOSONAR:  safe non-cryptographic random
 
 export const isEven = (value) => (value % 2 === 0) ? 'yes' : 'no';
