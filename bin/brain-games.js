c5cd7c7558dae992fa55fef9f64df0b35f785cf8
mammoth90 <max.mamontov90@gmail.com>
2025-10-17 14:17:21 +0500

    fix test falling

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,7 +3,7 @@ import { getValue as getName } from '../src/cli.js'
 
 
 console.log('Welcome to the Brain Games!');
-const question = 'May I ask your name? ';
+const question = 'May I have your name?';
 export const userName = getName(question);
 console.log(`Hello, ${userName}!`);
 
